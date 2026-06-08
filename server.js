8f8fc101c9c4f4995df8e9a30b8249439098a84c
juul <jdavelaar0@gmail.com>
2026-06-08 11:36:21 +0200

    #10 compression en data 1 voor 1 ophalen in get route

--- server.js
+++ server.js
@@ -1,72 +1,75 @@
 // 1. server.js basis, dit staat altijd in het begin van server.js, het is de soort van installatie ervan
 // dependencies die via npm install geïnstalleerd zijn 
-import express, { response, urlencoded } from 'express'
-import { Liquid } from 'liquidjs';
+import express from 'express'
+import compression from 'compression'
+import { Liquid } from 'liquidjs'
 
-// fetchen van data (je API)
-const getData = await fetch ('https://pokeapi.co/api/v2/pokemon?limit=151')
-
-// met dat fetchen zelf kan je niks dus je leest de JSON data van die fetch af, dit wordt dus je html/liquid. iets waar we wat mee kunnen
-const getDataJSON = await getData.json()
-
-// console.log(getDataJSON)
 //maak een express applicatie aan waarmee we de server configureren 
 const app = express()
 
 // dit zorgt ervoor dat ingevulde formulierdata leesbaar wordt dus als een gebruiker een form invult kan je dit uitlezen met request.body
+app.use(compression())
 app.use(express.urlencoded({extended: true}))
 
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts) Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static('public'))
 
-// start de liquid template motor op
+
+// start de liquid template engine op
 const engine = new Liquid();
-
-// koppelt die motor aan express zodat express .liquid bestanden kan verwerken
 app.engine('liquid', engine.express());
-
-// verteld waar templates bestanden staan, in map views
 app.set('views', './views')
-
-// zegt: standaard zijn mijn templates van het type liquid, daarom kun je response.render('index') doen zonder .liquid erachter
 app.set('view engine', 'liquid')
 
-// deze const haalt de images uit de API
-// pak mijn lijst van 151 pokemon en stuur deze over de lopende band. item = de pokémon die nu op de band ligt. index = het nummer van zijn plek
-// promise.all laat wachten op alle 151 detail fetches voor we verder gaan.
-const pokemon = await Promise.all(
-    getDataJSON.results.map(async function(item, index) {
-// berekenen van id bulbasaur ligt op plek 0 dus 0+1 = 1 enz. 
-    const id = index + 1 
-
-// Nieuwe regel voor de types zorgt ervoor de we in deze functie wachten op de fetch van de details. betekent: haal alle data van deze pokémon op
-    const detailResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-// data uitlezen
-    const detail = await detailResponse.json()
-// De nieuwe versie die van de band af rolt. name = naam overnemen, id is nieuwe id toewijzen, image = de URL van afbeelding bouwen met id type is ophalen van type
-    return {
-        name: item.name,
-        id: id,
-        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
-// detail.types is de lijst types, [0] is het eerste type, .type.name is de naam ervan
-        type: detail.types[0].type.name,
-        types: detail.types,
-        stats: detail.stats,
-        height: detail.height,
-        weight: detail.weight   
-    }
-})
-)
 
 
 // als iemand de homepage(/) opvraagt, doe dan dit:
 app.get ('/', async function (request, response) {
+try {
+    //zoekterm van URL uitlezen (?search)
+    const search = (request.query.search || '').trim()
+
+    //lijst van 151 pokémon ophalen
+    const getData = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    const getDataJSON = await getData.json()
+
+    // lege lijst die we vullen met loop
+    let pokemon = []
+
+    //loop door resultaten index begint bij - dus +1
+    for (const [index, item] of getDataJSON.results.entries()) {
+        const id = index + 1 
+
+    //per pokémon details ophalen
+    const detailResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const detail = await detailResponse.json()
+
+    //pokémon toevoegen aan lijst
+    pokemon.push({
+        name: item.name,
+        id: id,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+        // detail.types is de lijst types, [0] is het eerste type, .type.name is de naam ervan
+        type: detail.types[0].type.name,
+        types: detail.types,
+        stats: detail.stats,
+        height: detail.height,
+        weight: detail.weight  
+    })
+}
+
+//filteren op zoekterm
+if (search.length > 0) {
+    pokemon = pokemon.filter(function (item) {
+        return item.name.includes(search.toLowerCase())
+    })
+}
 
 // maak index.liquid om naar html en geef hier onderstaande data aan mee
-    response.render('index', {
-// de data die in de template mag, in me liquid kan ik dan {{} pokemon }} gebruiken
-    pokemon: pokemon
-    })
+    response.render('index', { pokemon: pokemon, search: search})
+ } catch (error) {
+    response.redirect('/?error=true')
+ }
 })
 
 
@@ -75,19 +78,31 @@ app.get ('/', async function (request, response) {
 app.get ('/pokemon/:id', async function (request, response) {
 
 // id uit URL halen 
+try {
         const id = request.params.id
+        const detailResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+if (!detailResponse.ok) {
+    return response.status(404).render('404')
+}
 
-        const foundPokemon = pokemon.find(function(item) {
-            return item.id == id
-        })
+const detail = await detailResponse.json()
+const pokemon = { 
+        name: detail.name,
+        id: id,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+        // detail.types is de lijst types, [0] is het eerste type, .type.name is de naam ervan
+        type: detail.types[0].type.name,
+        types: detail.types,
+        stats: detail.stats,
+        height: detail.height,
+        weight: detail.weight  
+    }
 
-        response.render('detail', {
-            pokemon: foundPokemon
-        })
-    })
-
-
-
+        response.render('detail', { pokemon: pokemon})
+ } catch (error) {
+    response.redirect('/?error=true')
+ }
+ })
 
 
 app.set('port', process.env.PORT || 8000)
