f8431a0330a33d09493063679ad0017a5dcf8882
juul <jdavelaar0@gmail.com>
2026-06-20 11:50:38 +0200

    #22 dark mode kleuren + JS + stylesheet

--- public/main.js
+++ public/main.js
@@ -15,3 +15,21 @@
         })
     })
 }
+
+document.documentElement.classList.add('js');
+const root = document.documentElement;
+const toggle = document.querySelector('.theme-toggle')
+
+const saved = localStorage.getItem('theme');
+if (saved) {
+    root.dataset.theme = saved;
+}
+
+toggle.addEventListener('click', () => {
+    const prefersDark = matchMedia('(prefers-color-scheme: dark)').matches;
+    const current = root.dataset.theme || (prefersDark ? 'dark' : 'light');
+    const next = current === 'dark' ? 'light' : 'dark';
+
+    root.dataset.theme = next;
+    localStorage.setItem('theme', next);
+});
